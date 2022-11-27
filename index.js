90245f0dde8ed065d522d7df642c169a068ebb3a
khorshed-alam-kbd <khorshedalam.kbd@gamil.com>
2022-11-28 00:28:27 +0600

    platinam-server

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0zdruwc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: " UnAuthorized Access" })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: " Forbidden Access" })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
+async function run() {
+
+    try {
+        const usersCollection = client.db('PlatinamSwapDB').collection('users');
+        // const reviewCollection = client.db('kaPhotography').collection('reviews');
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
+            res.send({ token })
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.error(error));
+
+
+app.get('/', (req, res) => {
+    res.send('platinam-swap server is running')
+});
+
+app.listen(port, () => {
+    console.log(`platinam-swap server running on ${port}`);
+})
+
